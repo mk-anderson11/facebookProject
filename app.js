27fbae45e62981425bc6569beedac97a1e326ca6
mk-anderson11 <matt.anderson11@gmail.com>
2016-10-13 10:05:10 -0600

    Working Final Project

--- app.js
+++ app.js
@@ -79,13 +79,16 @@ angular.module('News', ['ui.router'])
   '$scope',
   '$stateParams',
   'postFactory',
-  function($scope, $stateParams, postFactory){
+  'userFactory',
+  function($scope, $stateParams, postFactory, userFactory){
     $scope.post = postFactory.posts[$stateParams.id];
     $scope.addComment = function(){
       if($scope.body === '') { return; }
       $scope.post.comments.push({
         body: $scope.body,
-        upvotes: 0
+        upvotes: 0,
+        user: userFactory.name,
+        url: userFactory.url
       });
       $scope.body = '';
     };
